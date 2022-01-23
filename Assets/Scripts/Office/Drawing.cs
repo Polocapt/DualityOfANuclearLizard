cbe207d961b4dc2202c0522e08e79e846f153215
sam <samzebest@gmail.com>
2022-01-22 22:22:28 -0500

    scribbling works

--- Assets/Scripts/Office/Drawing.cs
+++ Assets/Scripts/Office/Drawing.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Drawing : MonoBehaviour
 {
     Pencil pencil;
-
+    Renderer rend;
+    Texture2D texture;
+    Vector2 LastMousePos;
 
     // Start is called before the first frame update
     void Start()
     {
         pencil = GameObject.FindGameObjectWithTag("Pencil").GetComponent<Pencil>();
+        rend = GetComponent<Renderer>();
+        // duplicate the original texture and assign to the material
+        texture = Instantiate(rend.material.mainTexture) as Texture2D;
+        rend.material.mainTexture = texture;
+        
+    }
+
+
+    Vector2 WorldPosToTextureCoord(Vector3 input)
+    {
+        Vector2 coord = new Vector2(
+            (texture.width - texture.width * (input.x - transform.position.x + rend.bounds.size.x / 2) / rend.bounds.size.x),
+            (texture.height - texture.height * (input.z - transform.position.z + rend.bounds.size.z / 2) / rend.bounds.size.z)
+            );
+        
+        return coord;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit))
@@ -26,11 +46,23 @@ public class Drawing : MonoBehaviour
                 pencil.gameObject.transform.eulerAngles = pencil.drawingAngle;
                 pencil.gameObject.transform.position = hit.point;
 
+                Vector2 coord = WorldPosToTextureCoord(hit.point);
+
                 if (Input.GetMouseButton(0))
                 {
                     // mouse is pressed and over paper
-                    Debug.Log("YAAAA");
+                    
+                    if (LastMousePos == null) {
+                        LastMousePos = coord;
+                        return;
+                    }
+                    DrawLine(coord, LastMousePos, Color.black);
+                    texture.Apply();
+
+                    
                 }
+
+                LastMousePos = coord;
             }
             else
             {
@@ -39,5 +71,57 @@ public class Drawing : MonoBehaviour
                 pencil.gameObject.transform.eulerAngles = pencil.restAngle;
             }
         }
+    }
+
+    public void DrawLine(Vector2 p1, Vector2 p2, Color col)
+    {
+        Vector2 t = p1;
+        float frac = 1 / Mathf.Sqrt(Mathf.Pow(p2.x - p1.x, 2) + Mathf.Pow(p2.y - p1.y, 2));
+        float ctr = 0;
+
+        while ((int)t.x != (int)p2.x || (int)t.y != (int)p2.y)
+        {
+            t = Vector2.Lerp(p1, p2, ctr);
+            ctr += frac;
+
+            Vector2[] brushpixels = GetBrushPixels((int)t.x, (int)t.y);
+
+            foreach (Vector2 pixel in brushpixels)
+                texture.SetPixel((int)pixel.x, (int)pixel.y, col);
+
+            
+        }
+    }
+
+    Vector2[] GetBrushPixels (int inputx, int inputy)
+    {
+        /*
+        int brushSize = 5;
+        int halfBrushSize = brushSize / 2;
+        Vector2 point = new Vector2(inputx, inputy);
+        List<Vector2> result = new List<Vector2>();
+
+        for(int x = inputx-halfBrushSize; x<inputx+halfBrushSize; x++)
+        {
+            for(int y=inputy-halfBrushSize; y<inputy+halfBrushSize; y++)
+            {
+                Vector2 pix = new Vector2(x, y);
+                if ((pix - point).magnitude < halfBrushSize) result.Add(pix);
+
+            }
+        }
+
+        return result.ToArray();
+
+        */
+
+        return new Vector2[]
+        {
+            new Vector2(inputx, inputy),
+            new Vector2(inputx-1, inputy),
+            new Vector2(inputx+1, inputy),
+            new Vector2(inputx, inputy-1),
+            new Vector2(inputx, inputy+1)
+        };
     }
 }
