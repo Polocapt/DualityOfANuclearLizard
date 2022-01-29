440edfc8fc5ed1febb343fe7bece40f250507e04
Polocapt <19712618+Polocapt@users.noreply.github.com>
2022-01-29 19:17:15 -0400

    Add URP

--- Assets/Scripts/City/LaserBeam.cs
+++ Assets/Scripts/City/LaserBeam.cs
@@ -3,24 +3,17 @@ using UnityEngine;
 public class LaserBeam : MonoBehaviour
 {
     
-    [SerializeField] private LineRenderer _lineRenderer;
     private float _laserRange = 0;
 
-    public void Init(float range, float startWidth, float endWidth)
+    public void Init(float range)
     {
         Stop();
         _laserRange = range;
-        _lineRenderer.startWidth = startWidth;
-        _lineRenderer.endWidth = endWidth;
     }
 
     private void Update()
     {
-        _lineRenderer.SetPosition(0, transform.position);
-        Vector3 endPos = transform.position + transform.forward * _laserRange;
-        _lineRenderer.SetPosition(1, endPos);
-        
-        if (Physics.SphereCast(transform.position,  3,transform.forward, out RaycastHit hit))
+        if (Physics.SphereCast(transform.position,  3,transform.forward,out RaycastHit hit, _laserRange))
         {
             if (hit.collider != null)
             {
