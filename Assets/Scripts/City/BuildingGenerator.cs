f5b3a7cadedefc37e073b95977d5efa617e91369
Polocapt <19712618+Polocapt@users.noreply.github.com>
2022-01-29 16:33:53 -0400

    Randomize building materials

--- Assets/Scripts/City/BuildingGenerator.cs
+++ Assets/Scripts/City/BuildingGenerator.cs
@@ -4,6 +4,7 @@ public class BuildingGenerator : MonoBehaviour
 {
 
     [SerializeField] private GameObject _building = null;
+    [SerializeField] private Renderer _renderer = null;
     public void GenerateBuilding()
     {
         int random = Random.Range(0, 100);
@@ -16,7 +17,10 @@ public class BuildingGenerator : MonoBehaviour
             return;
         }
         
-        Renderer rend = _building.GetComponent<Renderer>();
-        rend.material.color = Random.ColorHSV(0.5f, .55f, 0.1f, 0.5f, 0.1f, 0.4f);
+        _renderer.materials[0].color = Random.ColorHSV(0.45f, .55f, 0.1f, 0.4f, 0.1f, 0.4f);
+        
+        Color windowsColor = Random.ColorHSV(0f, 1f, 0.4f, 0.9f, 0.6f, 0.85f);
+        _renderer.materials[1].color = windowsColor;
+        _renderer.materials[1].SetColor("_EmissionColor", windowsColor);
     }
 }
