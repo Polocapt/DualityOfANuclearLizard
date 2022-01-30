3c84671edd570a339f99b24901b93865409ca02e
Polocapt <19712618+Polocapt@users.noreply.github.com>
2022-01-30 11:33:49 -0400

    Add background switch in main menu

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -1,23 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menu : MonoBehaviour
 {
+    [SerializeField] private GameObject _background;
+    [SerializeField] private float _fadeDuration = 4f;
+    [SerializeField] private float _holdDuration = 5f;
     public GameObject HowToPlay;
     public GameObject HowToPart1;
     public GameObject HowToPart2;
     bool page1 = true;
     bool HowToOpen = false;
 
-    // Start is called before the first frame update
+    private Image _image;
+
     void Start()
     {
-        
+        _image = _background.GetComponent<Image>();
+        StartCoroutine(SwitchBackground());
     }
 
-    // Update is called once per frame
+    private IEnumerator SwitchBackground()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_holdDuration);
+            Color c = _image.color;
+            c.a = 1;
+            float alpha = c.a;
+            while (alpha > 0)
+            {
+                alpha -= Time.deltaTime/_fadeDuration;
+                c.a = alpha;
+                _image.color = c;
+                yield return new WaitForEndOfFrame();
+            }
+            
+            yield return new WaitForSeconds(_holdDuration);
+            
+            c.a = 0;
+            alpha = c.a;
+            while (alpha < 1f)
+            {
+                alpha += Time.deltaTime/_fadeDuration;
+                c.a = alpha;
+                _image.color = c;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+    
     void Update()
     {
         if (HowToOpen)
