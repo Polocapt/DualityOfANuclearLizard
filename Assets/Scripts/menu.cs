a7c1ce565ba8a3856009789a184c7e8c14e86d32
sam <samzebest@gmail.com>
2022-01-30 09:36:27 -0500

    main menu how to page

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    public GameObject HowToPlay;
+    public GameObject HowToPart1;
+    public GameObject HowToPart2;
+    bool page1 = true;
+    bool HowToOpen = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,13 @@ public class menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (HowToOpen)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ToggleHowToPage();
+            }
+        }
         
     }
 
@@ -29,8 +42,34 @@ public class menu : MonoBehaviour
         Rage.value = 0;
     }
 
-    public void OpenOptions()
+    public void OpenHowTo()
     {
+        HowToPlay.SetActive(true);
+        HowToPart1.SetActive(true);
+        HowToPart2.SetActive(false);
+        page1 = true;
+        HowToOpen = true;
+    }
 
+    public void CloseHowTo()
+    {
+        HowToPlay.SetActive(false);
+        HowToOpen = false;
+    }
+
+    public void ToggleHowToPage()
+    {
+        if (page1)
+        {
+            HowToPart1.SetActive(false);
+            HowToPart2.SetActive(true);
+        }
+        else
+        {
+            HowToPart1.SetActive(true);
+            HowToPart2.SetActive(false);
+        }
+
+        page1 = !page1;
     }
 }
