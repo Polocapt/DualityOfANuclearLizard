ec0ab3b15cf88ec50a09d3b9bf3070e67269ee9c
sam <samzebest@gmail.com>
2022-01-30 11:50:30 -0500

    volumes carries forward

--- Assets/Sound/volumeSlider.cs
+++ Assets/Sound/volumeSlider.cs
@@ -8,15 +8,19 @@ public class volumeSlider : MonoBehaviour
 {
     public AudioMixer MasterMixer;
     Slider VolumeSlider;
+    static float level = 0f;
+    static float value = 1f;
     // Start is called before the first frame update
     void Start()
     {
         VolumeSlider = GetComponent<Slider>();
+        VolumeSlider.value = value;
     }
     
     public void UpdateMainVolume()
     {
-        float level = 1f- VolumeSlider.value;
+        value = VolumeSlider.value;
+        level = 1f- VolumeSlider.value;
         level = - 80f* Mathf.Pow(level, 3);
         MasterMixer.SetFloat("MainVolume", level);
     }
